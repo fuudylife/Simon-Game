84ddc26bf0a734e33f291e78fdb26092bc0eb03b
Fujikun <fujisawa3838@gmail.com>
2022-04-20 17:41:59 +0900

    ランダムに選ばれた色を配列に追加

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
+var gamePattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 }
 
