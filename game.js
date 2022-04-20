aa956b53eef480b03ff2dc6241e2558b77156a0c
Fujikun <fujisawa3838@gmail.com>
2022-04-20 18:30:49 +0900

    ランダムに選ばれた色のサウンドを再生

--- game.js
+++ game.js
@@ -8,4 +8,30 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    switch (randomChosenColour) {
+        case "blue":
+            var blue = new Audio("sounds/blue.mp3");
+                blue.play();
+            break;
+
+        case "green":
+            var green = new Audio("sounds/green.mp3");
+                green.play();
+            break;
+
+        case "red":
+            var red = new Audio("sounds/red.mp3");
+                red.play();
+            break;
+
+        case "yellow":
+            var yellow = new Audio("sounds/yellow.mp3");
+                yellow.play();
+            break;
+    
+        default:
+            console.log(randomChosenColour);
+            break;
+    }
 }
