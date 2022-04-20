f5ca53511bd736587605de7cc32ee05a9e0ac5b3
Fujikun <fujisawa3838@gmail.com>
2022-04-20 18:07:37 +0900

    ランダムに選ばれた色をフラッシュ点滅

--- game.js
+++ game.js
@@ -6,5 +6,6 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-}
 
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
